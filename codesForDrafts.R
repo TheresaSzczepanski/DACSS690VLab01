622259b32a97bead2dff3a62d6d2fee53fe4ab56
Theresa Szczepanski <theresaszczepanski@gmail.com>
2024-02-16 12:39:36 -0500

    Updated flexdashboard with draft deliverable 1

--- codesForDrafts.R
+++ codesForDrafts.R
@@ -2,6 +2,18 @@
 # clean memory ------------------------------------------------------------
 rm(list = ls())
 
+# load libraries -----------------------------------------------------------
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(readxl)
+library(hrbrthemes)
+library(viridis)
+library(ggpubr)
+library(purrr)
+library(plotly)
+library(dplyr)
+library(RColorBrewer)
 
 # read in data ------------------------------------------------------------
 #set working directory
@@ -9,25 +21,94 @@ rm(list = ls())
 filename="theFile.csv"
 mydata=read.csv(filename)
 
+filename2 = "_data/PrivProtectWinterGrowthAddress.csv"
+mydata2 <- read.csv(filename2)
+#view(mydata2)
+mydata2<- mydata2%>%
+  mutate(`Test 2 Benchmark Category` = recode_factor(`Test.2.Benchmark.Category`,
+                                                     "At/Above Benchmark" = "At/Above Benchmark",
+                                                     "On Watch" = "On Watch",
+                                                     "Intervention" = "Intervention",
+                                                     "Urgent Intervention"= "Urgent Intervention",
+                                                     .ordered = TRUE))%>%
+  mutate (`Test 2 Benchmark Category` = na_if(`Test.2.Benchmark.Category`, "-"))%>%
+  mutate(`Test 1 Benchmark Category` = recode_factor(`Test.1.Benchmark.Category`,
+                                                     "At/Above Benchmark" = "At/Above Benchmark",
+                                                     "On Watch" = "On Watch",
+                                                     "Intervention" = "Intervention",
+                                                     "Urgent Intervention"= "Urgent Intervention",
+                                                     .ordered = TRUE))%>%
+  mutate (`Test 1 Benchmark Category` = na_if(`Test.1.Benchmark.Category`, "-"))%>%
+  mutate(`Growth Proficiency Category` = recode_factor(`Growth.Proficiency.Category`,
+                                                       "High Growth, High Proficiency" = "High Growth, High Proficiency",
+                                                       "High Growth, Low Proficiency" = "High Growth, Low Proficiency",
+                                                       "Low Growth, High Proficiency" = "Low Growth, High Proficiency",
+                                                       "Low Growth, Low Proficiency" = "Low Growth, Low Proficiency",
+                                                       .ordered = TRUE))%>%
+  mutate(`Assignment Type` = `Assignment.Type`)%>%
+  mutate(`Postal Code` = `Postal.Code`)%>%
+  mutate(`Test 1 PR` = `Test.1.PR`)%>%
+  mutate(`Test 2 PR` = `Test.2.PR`)%>%
+  mutate(`SGP (Expectation=50)` = SGP..Expectation.50.)%>%
+  mutate(`TS Placement` = TS.Placement)%>%
+  select(`SASID`, `City`, `Postal Code`, `Grade`, `IEP`, `Assignment Type`, `Test 1 Benchmark Category`, `Test 1 PR`, `Test 2 Benchmark Category`, `Test 2 PR`, `Growth Proficiency Category`, `SGP (Expectation=50)`, `TS Placement`)
+
+mydata2<-na.omit(mydata2)
+#view(mydata2)
+
 
 # see data ----------------------------------------------------------
 
 
-head(mydata)
+head(mydata2)
 
 
 # see data types ----------------------------------------------------------
 
-str(mydata)
+str(mydata2)
 
 # deliverable 1 ----------------------------------------------------------
+Fall_Benchmark_Dist_Count<- mydata2%>%
+  mutate(student_count = 1)%>%
+  group_by(`Grade`, `Assignment Type`)%>%
+  summarize(student_total = sum(student_count))
+
+#view(Fall_Benchmark_Dist_Count)
+
+Fall_Benchmark_Dist <- mydata2%>%
+  filter(`Test 1 Benchmark Category` == "At/Above Benchmark")%>%
+  mutate(student_count = 1)%>%
+  select( `Assignment Type`, `Grade`,  `student_count`)%>%
+  group_by(`Grade`, `Assignment Type`)%>%
+  summarize(`Num Students` = sum(student_count))%>%
+  left_join(Fall_Benchmark_Dist_Count, by = c("Assignment Type" = "Assignment Type", "Grade" = "Grade"))%>%
+  mutate(`% Students` = 100*round(`Num Students`/student_total, 2))
+
+#view(Fall_Benchmark_Dist)
+
 library(ggplot2)
+base2 = ggplot(data = Fall_Benchmark_Dist, aes(x=`Grade`,fill = `Grade`, y = `% Students`))
+
+del1 = base2 + geom_bar(position="dodge", stat = "identity") +
+  facet_wrap(~`Assignment Type`)+
+  geom_text(aes( y = `% Students`, label = `% Students`,
+                 vjust = -.25))+
+  # scale_fill_brewer(palette = "Blues")+
+  #theme(axis.title.x=element_blank(),
+  #    axis.text.x=element_blank(),
+  #   axis.ticks.x=element_blank())+
+  labs(
+    y = "% Students At/Above Benchmark",
+    x= "Grade",
+    title = "STAR At/Above Benchmark Distribution",
+    caption = "Fall Screening")
 
 base= ggplot(data=mydata) 
 del1Draft= base + geom_bar(aes(x=LocaleType))
 del1Draft
 
-
+# save del1
+saveRDS(del1, file = "del1.rds")
 # save del1Draft ----------------------------------------------------------
 saveRDS(del1Draft, file = "del1Draft.rds")
 
